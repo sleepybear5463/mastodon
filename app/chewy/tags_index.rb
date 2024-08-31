bac72b77e03881de9f5e80c0cbcba30c78e10866
Dongmin Kim <rocky112358@gmail.com>
2024-08-31 23:03:48 +0900

    FIX: add tokenizer settings

--- app/chewy/tags_index.rb
+++ app/chewy/tags_index.rb
@@ -4,6 +4,15 @@ class TagsIndex < Chewy::Index
   include DatetimeClampingConcern
 
   settings index: index_preset(refresh_interval: '30s'), analysis: {
+    char_filter: {
+      tsconvert: {
+        type: 'stconvert',
+        keep_both: false,
+        delimiter: '#',
+        convert_type: 't2s',
+      },
+    },
+    
     analyzer: {
       content: {
         tokenizer: 'keyword',
@@ -14,7 +23,7 @@ class TagsIndex < Chewy::Index
           cjk_width
         ),
       },
-      
+
       char_filter: %w(tsconvert),
 
       edge_ngram: {
