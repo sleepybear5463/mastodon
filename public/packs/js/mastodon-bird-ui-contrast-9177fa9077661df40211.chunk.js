efe2ee23a23429bd87e21d66521a74d8455ca7e7
Dongmin Kim <rocky112358@gmail.com>
2024-09-03 00:04:01 +0900

    FIX: update bird-ui

---
+++ public/packs/js/mastodon-bird-ui-contrast-9177fa9077661df40211.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[579],{1063:function(n,w,o){"use strict";o.r(w)}},[[1063,0]]]);
+//# sourceMappingURL=mastodon-bird-ui-contrast-9177fa9077661df40211.chunk.js.map
---
+++ public/packs/js/mastodon-bird-ui-dark-67066bd41cdfc922d55c.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[580],{1064:function(n,w,o){"use strict";o.r(w)}},[[1064,0]]]);
+//# sourceMappingURL=mastodon-bird-ui-dark-67066bd41cdfc922d55c.chunk.js.map
